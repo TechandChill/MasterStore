ed41d6f6e5dc4b0aa99cb7bea72d42a9866a253c
HGE-studio <57325674+HGE-studio@users.noreply.github.com>
2020-11-19 10:22:04 +0100

    Add files via upload

--- Releases/0.1/main.py
+++ Releases/0.1/main.py
@@ -1,26 +1,22 @@
-from tkinter import *
+import htmlPy
+import os
+
+app = htmlPy.AppGUI(title=u"Project MasterStore-Dev", maximized=True)
+
+app.template_path = os.path.abspath(".")
+app.static_path = os.path.abspath(".")
+
+app.template = ("index.html", {"username": "htmlPy_user"})
 
 from art import text2art
 
-import config
-import update
+import json
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
 
 print(text2art("MasterStore"))
 print("App started.")
 
-
-root = Tk() 
-  
-root.title("Project MasterStore-Dev") 
-root.geometry('900x720')
-
-if(config.check_for_updates == True):
-    update.check_version()
-else:
-    pass
-
-title = Label(root, text = f"Project Masterstore v{config.version}", fg="#f93434", font=("Helvetica", 18))
-title.grid(row=0, column=0)
-
-
-root.mainloop()
+app.start()
--- Releases/0.1/update.py
+++ Releases/0.1/update.py
@@ -1,9 +1,12 @@
-from tkinter import messagebox
-import config
+import json
+
+import json
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
 
 def check_version():
-    if(config.version >= config.latest_version):
-        messagebox.showinfo("Info", "Masterstore is updated.")
+    if(config["version"] >= config["latest_version"]):
+        pass
     else:
-        messagebox.showwarning("Warning", f"MasterStore is not up-to-date\nYou should upgrade to have access to more apps and a\nbetter experience.\n\nYour version: {config.version}.\n\nThe latest version: {config.latest_version}.")
-
+        pass
